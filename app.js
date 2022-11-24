903db1677f089c8e179b45c1f840ad534c4ac608
jatin kumar <jatins9227@gmail.com>
2022-11-25 00:40:08 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint version:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+  console.log("server started!");
+});
