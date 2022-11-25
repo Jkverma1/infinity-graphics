040f06a41f8cdc3edde9bbd0fee83f3f2b298ff3
jatin kumar <jatins9227@gmail.com>
2022-11-25 13:32:09 +0530

    static

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(process.env.PORT || 3000, (req, res) => {
+app.listen(process.env.PORT || 5000, (req, res) => {
   console.log("server started!");
 });
